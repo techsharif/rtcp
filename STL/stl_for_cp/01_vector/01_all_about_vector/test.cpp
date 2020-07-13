d57497368f7014646d5864f95dadb222c8ee1389
Md. Sharif Chowdhury <sharif.cse.hstu@gmail.com>
2020-07-13 21:57:51 +0600

    add test file

---
+++ STL/stl_for_cp/01_vector/01_all_about_vector/test.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {2, 4, 5, 8, 9};
+    int p, d;
+
+    cin >> p >> d; /// input 2, 7
+
+    /// your code start
+
+
+    /// your code end
+
+    for (int a : arr)
+        cout << a << ", ";
+
+    /// output
+    /// 2, 4, 7, 5, 8, 9
+
+    return 0;
+}
